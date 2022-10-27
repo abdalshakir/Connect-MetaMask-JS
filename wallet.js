f9e2f32622a6c284252e2eaa43df2ba1a2cf7946
Abdal Shakir <abdal_shakir715@hotmail.com>
2022-10-27 10:18:48 +0500

    show tx hash

--- wallet.js
+++ wallet.js
@@ -1,6 +1,7 @@
 const connectButton = document.getElementById("connect-button");
 const currentAccount = document.getElementById("current-account");
 const sendEthers = document.getElementById("send-ethers");
+const showTxHash = document.getElementById("tx-hash");
 
 let isWalletConnected;
 console.log(isWalletConnected);
@@ -12,9 +13,6 @@ async function connectWallet() {
             let accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
             currentAccount.innerText = accounts[0];
             isWalletConnected = ethereum.isConnected();
-            ethereum.on('accountsChanged', (acc) => {
-                currentAccount.innerText = acc[0];
-            });
         } catch (error) {
             console.log(error);
             if (error.code === 4001) {
@@ -24,7 +22,7 @@ async function connectWallet() {
             }
         }
 
-        // Switch to Ethereum Chain
+        // Switch to Goerli Chain
         if (isWalletConnected) {
             try {
                 await window.ethereum.request({ method: "wallet_switchEthereumChain", params: [{ chainId: '0x5' }] });
@@ -45,12 +43,13 @@ async function sendTransaction() {
         const transactionParameters = {
             gasPrice: Number(40 * 1000000000).toString(16), //'0x09184e72a000'  
             gas: Number(21000).toString(16), //'0x5208'
-            to: '0x018b28eA884a0988E5d331Aa292443e496238429',
+            to: '0x5FA0be692bC49A68510F42A747522006cc49c720',
             from: ethereum.selectedAddress,
-            value: Number(0.048 * 1000000000000000000).toString(16)
+            value: Number(0.047 * 1000000000000000000).toString(16)
         };
         try {
             let txHash = await ethereum.request({ method: "eth_sendTransaction", params: [transactionParameters] });
+            showTxHash.innerText = txHash;
             console.log("Transaction Hash: " + txHash);
         } catch (error) {
             console.log(error)
@@ -61,3 +60,8 @@ async function sendTransaction() {
 }
 
 sendEthers.addEventListener('click', sendTransaction);
+
+
+ethereum.on('accountsChanged', (acc) => {
+    currentAccount.innerText = acc[0];
+});
